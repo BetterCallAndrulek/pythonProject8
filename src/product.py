ab622acdca48ad9504c5f93e1c74698fdbcd6165
BetterCallAndrulek <156514407+BetterCallAndrulek@users.noreply.github.com>
2024-02-13 23:33:34 +0700

    Update product.py

--- src/product.py
+++ src/product.py
@@ -4,10 +4,10 @@ class Product:
     price: float
     quantity: int
 
-    def __init__(self, name, description, price, quantity):
+    def __init__(self, name, description, _price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self._price = _price
         self.quantity = quantity
 
     def get_name(self):
@@ -16,8 +16,31 @@ class Product:
     def get_description(self):
         return self.description
 
-    def get_price(self):
-        return self.price
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            print("Цена введена не корректно")
+        elif value < self._price:
+            while True:
+                answer = input("Новая цена ниже чем старая, вы уверены что хотите изменить цену (y/n): ").lower()
+                if answer == "y":
+                    self._price = value
+                    break
+                elif answer == "n":
+                    self._price = self._price
+                    break
+        else:
+            self._price = value
 
     def get_quantity_in_stock(self):
         return self.quantity
+
+    @classmethod
+    def create_product(cls, product):
+        if product["name"] == Product.name:
+            Product.quantity += product["quantity"]
+            return product
