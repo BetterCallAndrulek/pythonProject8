aeadd4d810c44c402008b18ae3d1e850f2e98971
BetterCallAndrulek <156514407+BetterCallAndrulek@users.noreply.github.com>
2024-02-13 23:31:46 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,28 +4,37 @@ from src.product import Product
 
 
 def main():
-    file = open_file()
-    result = []
-    for elem in file:
-        list_products = [el["name"] for el in elem["products"]]
-        category = Category(elem["name"], elem["description"], list_products)
-        result.append(f"{category.get_name()}\n"
-                      f"{category.get_description()}\n"
-                      f"{category.get_products()}\n\n")
+    c1 = Category("Смартфоны", "Смартфоны", [
+        {
+            "name": "Samsung Galaxy C23 Ultra",
+            "description": "256GB, Серый цвет, 200MP камера",
+            "price": 180000.0,
+            "quantity": 5
+        },
+        {
+            "name": "Iphone 15",
+            "description": "512GB, Gray space",
+            "price": 210000.0,
+            "quantity": 8
+        },
+        {
+            "name": "Xiaomi Redmi Note 11",
+            "description": "1024GB, Синий",
+            "price": 31000.0,
+            "quantity": 14
+        }
+    ])
 
-        full_list_products = [el for el in elem["products"]]
-        for el in full_list_products:
-            products = Product(el["name"], el["description"], el["price"], el["quantity"])
-            result.append(f"{products.get_name()}\n"
-                          f"{products.get_description()}\n"
-                          f"{products.get_price()}\n"
-                          f"Количество в наличии - {products.get_quantity_in_stock()}\n\n")
+    p1 = Product("Iphone 15", "512GB, Gray space", 210_000.0, 8)
+    print(c1.products)
 
-    result.append(f"Количество категорий - {category.number_categories}\n"
-                  f"Количество товаров - {category.number_products}")
-
-    return "".join(result)
+    c1.products = Product.create_product({"name": "Iphone 15",
+                                          "description": "512GB, Gray space",
+                                          "price": 210000.0,
+                                          "quantity": 8
+                                          })
+    print(c1.products)
 
 
-if __name__ == '__main__':
-    print(main())
+if __name__ == "__main__":
+    main()
