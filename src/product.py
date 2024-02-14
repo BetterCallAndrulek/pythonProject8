43fdba7b5a559798fd2a74d7ab7c4cacf1b537ad
BetterCallAndrulek <156514407+BetterCallAndrulek@users.noreply.github.com>
2024-02-14 10:09:04 +0700

    Update product.py

--- src/product.py
+++ src/product.py
@@ -1,13 +1,8 @@
 class Product:
-    name: str
-    description: str
-    price: float
-    quantity: int
-
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self._price = price
         self.quantity = quantity
 
     def get_name(self):
@@ -16,8 +11,39 @@ class Product:
     def get_description(self):
         return self.description
 
-    def get_price(self):
-        return self.price
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            print("Цена введена не корректно")
+        elif value < self._price:
+            while True:
+                answer = input("Новая цена ниже чем старая, вы уверены что хотите изменить цену (y/n): ").lower()
+                if answer == "y":
+                    self._price = value
+                    break
+                elif answer == "n":
+                    self._price = self._price
+                    break
+        else:
+            self._price = value
 
     def get_quantity_in_stock(self):
         return self.quantity
+
+    @classmethod
+    def create_product(cls):
+        new_product = {
+            "name": "HUAWEI P60",
+            "description": "Инновационный изогнутый экран",
+            "price": 62_000.0,
+            "quantity": 19
+        }
+        name, description, price, quantity = \
+            (
+                new_product["name"], new_product["description"], new_product["price"], new_product["quantity"]
+            )
+        return cls(name, description, price, quantity)
