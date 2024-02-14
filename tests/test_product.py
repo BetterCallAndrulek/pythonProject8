ea44c5505dfd5d0320a9ef39d124e93bafaa1834
BetterCallAndrulek <156514407+BetterCallAndrulek@users.noreply.github.com>
2024-02-14 10:13:26 +0700

    Update test_product.py

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,14 +1,9 @@
-import pytest
 from src.product import Product
 
 
-@pytest.fixture()
-def class_product():
-    return Product("Телевизоры", "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником", 123000.0, 7)
-
-
-def test_classes_product(class_product):
-    assert class_product.get_name() == "Телевизоры"
-    assert class_product.get_description() == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
-    assert class_product.get_price() == 123000.0
+def test_classes_product(class_product, class_category):
+    assert class_product.get_name() == "55\" QLED 4K"
+    assert class_product.get_description() == "Фоновая подсветка"
+    assert class_product._price == 123000.0
     assert class_product.get_quantity_in_stock() == 7
+    assert Product.create_product() == ("HUAWEI P60", "Инновационный изогнутый экран", 62000.0, 19)
