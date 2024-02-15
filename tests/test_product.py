ef52822e9bc765137dac26fa34644666cdb0b7f2
BetterCallAndrulek <156514407+BetterCallAndrulek@users.noreply.github.com>
2024-02-15 12:32:41 +0700

    Update test_product.py

--- tests/test_product.py
+++ tests/test_product.py
@@ -4,6 +4,11 @@ from src.product import Product
 def test_classes_product(class_product, class_category):
     assert class_product.get_name() == "55\" QLED 4K"
     assert class_product.get_description() == "Фоновая подсветка"
-    assert class_product._price == 123000.0
+    assert class_product.price == 123000.0
     assert class_product.get_quantity_in_stock() == 7
-    assert Product.create_product() == ("HUAWEI P60", "Инновационный изогнутый экран", 62000.0, 19)
+    assert Product.create_product({
+        "name": "HUAWEI P60",
+        "description": "Инновационный изогнутый экран",
+        "price": 62_000.0,
+        "quantity": 19
+    })
