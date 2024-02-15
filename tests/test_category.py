5a829039a876734d1699358389da00ab104bc88c
BetterCallAndrulek <156514407+BetterCallAndrulek@users.noreply.github.com>
2024-02-15 12:34:58 +0700

    Update test_category.py

--- tests/test_category.py
+++ tests/test_category.py
@@ -5,8 +5,4 @@ def test_classes_product(class_category):
                                        "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
                                        "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n")
 
-    class_category.products = ("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    assert class_category.products == ("Samsung Galaxy C23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
-                                       "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
-                                       "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n"
-                                       "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n")
+    class_category.add_products(("Iphone 15", "512GB, Gray space", 210000.0, 8))
