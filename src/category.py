64501ff15dde1eb46df6c4f2a7af6ed7ca9434fd
BetterCallAndrulek <156514407+BetterCallAndrulek@users.noreply.github.com>
2024-02-15 12:30:38 +0700

    Update category.py

--- src/category.py
+++ src/category.py
@@ -1,8 +1,8 @@
-lass Category:
+class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self._products = products
+        self.__products = products
 
     def get_name(self):
         return self.name
@@ -13,14 +13,13 @@ lass Category:
     @property
     def products(self):
         list_product = []
-        for products in self._products:
+        for products in self.__products:
             list_product.append(f"{products["name"]}, {products["price"]} руб. Остаток: {products["quantity"]} шт.\n")
 
         return "".join(list_product)
 
-    @products.setter
-    def products(self, value):
-        self._products.append({
+    def add_products(self, value):
+        self.__products.append({
             "name": value[0],
             "description": value[1],
             "price": value[2],
