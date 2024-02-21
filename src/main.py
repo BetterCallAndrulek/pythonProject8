4f8886f9208bec65fdf81373fc17e24e6963049a
BetterCallAndrulek <ddemon245@gmail.com>
2024-02-21 22:28:35 +0700

    new

--- src/main.py
+++ src/main.py
@@ -1,40 +1,26 @@
-from src.utils import open_file
+import json
+
 from src.category import Category
 from src.product import Product
 
 
 def main():
-    c1 = Category("Смартфоны", "Смартфоны", [
-        {
-            "name": "Samsung Galaxy C23 Ultra",
-            "description": "256GB, Серый цвет, 200MP камера",
-            "price": 180000.0,
-            "quantity": 5
-        },
-        {
-            "name": "Iphone 15",
-            "description": "512GB, Gray space",
-            "price": 210000.0,
-            "quantity": 8
-        },
-        {
-            "name": "Xiaomi Redmi Note 11",
-            "description": "1024GB, Синий",
-            "price": 31000.0,
-            "quantity": 14
-        }
-    ])
-
-    p1 = Product("Iphone 15", "512GB, Gray space", 210_000.0, 8)
-    print(c1.products)
-
-    c1.products = Product.create_product({"name": "Iphone 15",
-                                          "description": "512GB, Gray space",
-                                          "price": 210000.0,
-                                          "quantity": 8
-                                          })
-    print(c1.products)
+    """
+    :return: Списки с информацией о категориях и товарах
+    """
+    with open(FILE_JSON) as file:
+        raw_json = file.read()
+        data = json.loads(raw_json)
+        categories = []
+        for cat_data in data:
+            products_instances = []
+            for product_data in cat_data['products']:
+                new_product = Product.new_product(product_data)
+                products_instances.append(new_product)
+            category = Category(cat_data['name'], cat_data['description'], products_instances)
+            categories.append(category)
+        print(categories)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
