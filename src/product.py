fbe398d41daa1f3e0d7bcd02176e6eef0aa95b6e
BetterCallAndrulek <156514407+BetterCallAndrulek@users.noreply.github.com>
2024-02-26 22:54:37 +0700

    Update product.py

--- src/product.py
+++ src/product.py
@@ -11,22 +11,21 @@ class Product:
         self.__price = price
         self.quantity = quantity
         self.color = color
-
+                     
     @classmethod
     def new_product(cls, product_data):
         return cls(**product_data)
-
     def cor_product(self, product_data):
         new_product = Product.new_product(product_data)
         if new_product.name == self.name:
             self.quantity += new_product.quantity
             if self.price <= new_product.price:
                 self.price = new_product.price
-
+                
     @property
     def price(self):
         return self.__price
-
+        
     @price.setter
     def price(self, new_price):
         if new_price <= 0:
@@ -37,10 +36,10 @@ class Product:
                 self.__price = new_price
         elif new_price > self.__price:
             self.__price = new_price
-
+            
     def __str__(self):
         return f'{self.name}, {int(self.__price)} руб. Остаток: {self.quantity} шт.'
-
+        
     def __add__(self, product):
         if type(product) != type(self):
             raise TypeError('Разные классы')
