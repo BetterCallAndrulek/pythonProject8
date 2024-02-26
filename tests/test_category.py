5383b610a9897e6210d971bedccc14476b3f922f
BetterCallAndrulek <156514407+BetterCallAndrulek@users.noreply.github.com>
2024-02-26 22:22:27 +0700

    Update test_category.py

--- tests/test_category.py
+++ tests/test_category.py
@@ -5,7 +5,7 @@ def test_init_category(category, smartphone, shop):
     assert category.name == "Смартфоны"
     assert category.description == "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни"
     assert category.total_number_of_unique_products == 2
-    category.add_product(smartphone)
+    category_fixture[0].add_product(category_fixture[1])
     assert category.products == ['Samsung Galaxy C23 Ultra, 180000 руб. Остаток: 5 шт.',
                                  'Iphone 15, 210000 руб. Остаток: 8 шт.',
                                  'OnePlus 10Pro 12/256Gb Volcan Bl, 150000 руб. Остаток: 15 шт.']
@@ -14,5 +14,5 @@ def test_init_category(category, smartphone, shop):
     assert category.__str__() == 'Смартфоны, количество продуктов: 3 шт.'
 
     with pytest.raises(TypeError):
-        category.add_product(shop)
+        category_fixture[0].add_product(category_fixture[0])
 
