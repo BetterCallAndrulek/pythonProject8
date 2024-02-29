0da1efaadf357224f28bc13c91a4b6e6a88551d3
BetterCallAndrulek <156514407+BetterCallAndrulek@users.noreply.github.com>
2024-02-29 09:47:54 +0700

    Update grass.py

--- src/grass.py
+++ src/grass.py
@@ -1,6 +1,5 @@
 from src.product import Product
-class LawnGrass(ProductsABC, ObjectMixin):
-
+class LawnGrass(Product, ObjectMixin):
     def __init__(self,
                  name: str,
                  description: str,
@@ -12,7 +11,3 @@ class LawnGrass(ProductsABC, ObjectMixin):
         super().__init__(name, description, price, quantity, color)
         self.country = country
         self.germination_period = germination_period
-
-    def keeping(self):
-
-        print('Газонная трава хранится в помещении при температуре не выше +10 градусов')
