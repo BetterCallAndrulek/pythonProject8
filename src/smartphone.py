4329077f717117c29dad179564a2016d94538d5b
BetterCallAndrulek <156514407+BetterCallAndrulek@users.noreply.github.com>
2024-02-29 19:20:14 +0700

    Update smartphone.py

--- src/smartphone.py
+++ src/smartphone.py
@@ -1,7 +1,8 @@
-from src.product import Product
-class Smartphone(ProductsABC, ObjectMixin):
+from object_mixin import ObjectMixin
+from product import Product
 
 
+class Smartphone(Product, ObjectMixin):
     def __init__(self,
                  name: str,
                  description: str,
@@ -11,11 +12,7 @@ class Smartphone(ProductsABC, ObjectMixin):
                  efficiency: str,
                  model: str,
                  memory: int):
-        super().__init__(name, description, price, quantity, color)
         self.efficiency = efficiency
         self.model = model
         self.memory = memory
-
-    def keeping(self):
-
-        print(f'Смартфон хранится в сухом месте при комнатной температуре')
+        super().__init__(name, description, price, quantity, color)
