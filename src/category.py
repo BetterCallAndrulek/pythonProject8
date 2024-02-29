10e4aed588d470d3a92dcb2c434f66cc3b9f7a10
BetterCallAndrulek <156514407+BetterCallAndrulek@users.noreply.github.com>
2024-02-29 09:47:05 +0700

    Update category.py

--- src/category.py
+++ src/category.py
@@ -1,13 +1,13 @@
 class Category(ObjectMixin, CategoryOrderABC):
-
-    
     total_number_of_categories = 0
     total_number_of_unique_products = 0
+
     def __init__(self, name: str, description: str, products: list):
         super().__init__()
         self.name = name
         self.description = description
         self.__products = products
+
         Category.total_number_of_categories += 1
         Category.total_number_of_unique_products += len(self.__products)
 
@@ -23,8 +23,10 @@ class Category(ObjectMixin, CategoryOrderABC):
         for prod in self.__products:
             product_info.append(f'{prod.name}, {int(prod.price)} руб. Остаток: {prod.quantity} шт.')
         return product_info
+
     def __len__(self):
         return len(self.__products)
+
     def __str__(self):
         return f'{self.name}, количество продуктов: {self.__len__()} шт.'
 
