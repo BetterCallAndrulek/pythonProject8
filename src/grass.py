5c835f5583251ae80eaf1c458909912847338ddd
BetterCallAndrulek <156514407+BetterCallAndrulek@users.noreply.github.com>
2024-02-29 19:16:24 +0700

    Update grass.py

--- src/grass.py
+++ src/grass.py
@@ -1,5 +1,9 @@
-from src.product import Product
+from object_mixin import ObjectMixin
+from product import Product
+
+
 class LawnGrass(Product, ObjectMixin):
+
     def __init__(self,
                  name: str,
                  description: str,
@@ -8,6 +12,6 @@ class LawnGrass(Product, ObjectMixin):
                  color: str,
                  country: str,
                  germination_period: int):
-        super().__init__(name, description, price, quantity, color)
         self.country = country
         self.germination_period = germination_period
+        super().__init__(name, description, price, quantity, color)
