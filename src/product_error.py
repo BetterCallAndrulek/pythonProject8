991d643d15f979d64a0963ce5e777554045562f5
BetterCallAndrulek <156514407+BetterCallAndrulek@users.noreply.github.com>
2024-03-05 19:03:32 +0700

    Update product_error.py

--- src/product_error.py
+++ src/product_error.py
@@ -1,4 +1,5 @@
 class ProductError(Exception):
+
     def __init__(self, *args):
         self.message = args if args else 'Неизвестная ошибка'
 
@@ -7,10 +8,12 @@ class ProductError(Exception):
 
 
 class ProductClassError(ProductError):
+
     def __init__(self, *args):
         self.message = args if isinstance(args, self.__class__) else 'Добавлять можно только объекты Product и дочерние от них'
 
 
 class ProductZeroError(ProductError):
+
     def __init__(self, *args):
         self.message = args if args != 0 else 'Добавлять можно только объекты Product и дочерние от них'
